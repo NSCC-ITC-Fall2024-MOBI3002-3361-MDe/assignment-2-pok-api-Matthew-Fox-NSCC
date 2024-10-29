6b61ec80154109c8c8aab7cddb3754f789542822
Matthew Fox <25204673+Parascythe42@users.noreply.github.com>
2024-10-29 12:09:56 -0300

    Added retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Adding dependencies
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
